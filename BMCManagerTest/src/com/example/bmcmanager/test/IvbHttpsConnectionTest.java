785dc5afc938d4035a50f55e236fe64cdc2cf872
Lucio Seki <lucioseki@gmail.com>
2014-10-10 14:44:25 -0300

    Test IvbHttpsConnection.connect() and .getSensors()
    connect() -> test invalid host IP and invalid login
    getSensors() -> test fans, temperatures and voltages

--- BMCManagerTest/src/com/example/bmcmanager/test/IvbHttpsConnectionTest.java
+++ BMCManagerTest/src/com/example/bmcmanager/test/IvbHttpsConnectionTest.java
@@ -1,28 +1,50 @@
 package com.example.bmcmanager.test;
 
+import java.io.IOException;
 import java.net.ConnectException;
 import java.util.List;
 
 import org.apache.http.auth.AuthenticationException;
 
 import android.test.AndroidTestCase;
-import com.solucionamos.bmcconnection.Fan;
 import com.solucionamos.bmcconnection.IvbHttpsConnection;
+import com.solucionamos.bmcconnection.Sensor;
 
 public class IvbHttpsConnectionTest extends AndroidTestCase {
 
 	// BMC IP address
 	private static final String BMC_IP = "10.35.101.51";
+	private static IvbHttpsConnection conn;
 
-	// throw Exception if host IP is invalid
-	public void testInvalidHostIp() {
-		Exception ex = null;
-		IvbHttpsConnection conn = new IvbHttpsConnection("255.255.255.255", "",
-				"");
-
+	// create a new connection to BMC
+	public void setUp() {
+		conn = new IvbHttpsConnection(BMC_IP, "lenovo", "len0vO");
 		try {
 			conn.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// disconnect from BMC
+	public void tearDown() {
+		try {
 			conn.disconnect();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	// throw Exception if host IP is invalid
+	public void testInvalidHostIp() throws Exception {
+		Exception ex = null;
+		IvbHttpsConnection conn2 = new IvbHttpsConnection("255.255.255.255",
+				"", "");
+
+		try {
+			conn2.connect();
+			conn2.disconnect();
 		} catch (Exception e) {
 			ex = e;
 		}
@@ -33,11 +55,11 @@ public class IvbHttpsConnectionTest extends AndroidTestCase {
 	// throw Exception if username/password is invalid
 	public void testInvalidAuthentitcation() throws Exception {
 		Exception ex = null;
-		IvbHttpsConnection conn = new IvbHttpsConnection(BMC_IP, "nologin", "");
+		IvbHttpsConnection conn2 = new IvbHttpsConnection(BMC_IP, "nologin", "");
 
 		try {
-			conn.connect();
-			conn.disconnect();
+			conn2.connect();
+			conn2.disconnect();
 		} catch (Exception e) {
 			ex = e;
 		}
@@ -45,24 +67,24 @@ public class IvbHttpsConnectionTest extends AndroidTestCase {
 		assertTrue(ex instanceof AuthenticationException);
 	}
 
-	// successfully connect to the BMC
-	public void testConnectToBMC() throws Exception {
-		IvbHttpsConnection conn = new IvbHttpsConnection(BMC_IP, "lenovo",
-				"len0vO");
-
-		conn.connect();
-		conn.disconnect();
-	}
-
 	// successfully get fan data
 	public void testGetFans() throws Exception {
-		IvbHttpsConnection conn = new IvbHttpsConnection(BMC_IP, "lenovo",
-				"len0vO");
-
-		conn.connect();
-		List<Fan> fans = conn.getFans();
-		assertTrue(fans.get(0) instanceof Fan);
+		List<Sensor> fans = conn.getSensors(Sensor.TYPE_FAN);
+		assertTrue(fans.get(0) instanceof Sensor);
 		assertEquals(fans.get(0).getUnits(), "RPM");
-		conn.disconnect();
+	}
+
+	// successfully get temperature data
+	public void testGetTemperatures() throws Exception {
+		List<Sensor> temperatures = conn.getSensors(Sensor.TYPE_TEMPERATURE);
+		assertTrue(temperatures.get(0) instanceof Sensor);
+		assertEquals(temperatures.get(0).getUnits(), "C");
+	}
+
+	// successfully get voltage data
+	public void testGetVoltages() throws Exception {
+		List<Sensor> voltages = conn.getSensors(Sensor.TYPE_VOLTAGE);
+		assertTrue(voltages.get(0) instanceof Sensor);
+		assertEquals(voltages.get(0).getUnits(), "V");
 	}
 }
